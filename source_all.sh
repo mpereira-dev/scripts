8b4f9c36ca4358227dfebc0700f2632f181a8d5c
Miguel Pereira <mpereira.dev@gmail.com>
2021-10-17 14:45:50 -0400

    source_all

---
+++ source_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Helper script to load aliases and functions in other .sh files.
+
+# https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
+export SCRIPTS_HOME="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export PATH=$PATH:"$SCRIPTS_HOME"
+
+# Source each .sh script in this dirctory or ./apps.
+function source_all {
+  # Generic
+  for SCRIPT in $(ls $SCRIPTS_HOME | grep sh); do
+    source $SCRIPTS_HOME/$SCRIPT
+  done
+  # App specific
+  for SCRIPT in $(ls $SCRIPTS_HOME/apps | grep sh); do
+    source $SCRIPTS_HOME/apps/$SCRIPT
+  done
+}
