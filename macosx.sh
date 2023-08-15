5080515ef2229cfc8a990e80fdaa1a16ddfa077d
Miguel Pereira <mpereira.dev@gmail.com>
2023-08-14 20:35:53 -0400

    Update macosx.sh script for x86 and arm

--- macosx.sh
+++ macosx.sh
@@ -2,9 +2,16 @@
 export PATH="/Users/$(whoami)/.jenv/bin:$PATH"
 
 # jenv, direnv, shellenv
-eval "$(/opt/homebrew/bin/brew shellenv)"
-eval "$(/opt/homebrew/bin/direnv hook bash)"
-eval "$(/opt/homebrew/bin/jenv init -)"
+if [[ $(uname -m) == 'arm' ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  eval "$(/opt/homebrew/bin/direnv hook bash)"
+  eval "$(/opt/homebrew/bin/jenv init -)"
+else
+  eval "$(/usr/local/bin/brew shellenv)"
+  eval "$(/usr/local/bin/direnv hook bash)"
+  eval "$(/usr/local/bin/jenv init -)"
+fi
+
 
 # https://code2care.org/q/the-default-interactive-shell-is-now-zsh-macos
 export BASH_SILENCE_DEPRECATION_WARNING=1
